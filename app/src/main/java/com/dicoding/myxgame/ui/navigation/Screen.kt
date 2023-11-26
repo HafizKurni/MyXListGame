e8dd033a5dfdf1464be0f57ffc9ccd479eb0ccea
Hafiz Muhammad Kurniawan <87954942+HafizKurni@users.noreply.github.com>
2023-11-26 07:10:41 +0700

    Update Screen.kt
    Benerin nama parameter

--- app/src/main/java/com/dicoding/myxgame/ui/navigation/Screen.kt
+++ app/src/main/java/com/dicoding/myxgame/ui/navigation/Screen.kt
@@ -3,7 +3,7 @@ package com.dicoding.myxgame.ui.navigation
 sealed class Screen(val route: String) {
     object Home : Screen("home")
     object About : Screen("about")
-    object Detail : Screen("home/{id}") {
+    object Detail : Screen("home/{gameId}") {
         fun createRoute(gameId: Long) = "home/$gameId"
     }
 }
